a81aa14f43d4092bab57fc7c2574f6ebd3d3a5ca
Tomaeto <adrianspenser@gmail.com>
2024-06-05 23:27:40 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use ((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mh4udb.netlify.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mh4udb.netlify.app');
     res.setHeader('Content-Type', 'application/json');
     next();
 })
